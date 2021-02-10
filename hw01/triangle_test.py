70edee9840c0585e5610ec35977376f5ffe44eea
kylemcshea <kmcshea@stevens.edu>
2021-02-09 23:12:09 -0500

    added more tests to test script

--- hw01/triangle_test.py
+++ hw01/triangle_test.py
@@ -13,9 +13,12 @@ class Test(unittest.TestCase):
     def test01(self):
         self.assertEqual(triangle.classify_triangle(10,10,10),"equilateral")
         self.assertEqual(triangle.classify_triangle(10,5,3),"scalene")
+        self.assertEqual(triangle.classify_triangle(100,8,3),"scalene")
         self.assertEqual(triangle.classify_triangle(10,3,3),"isosceles")
+        self.assertEqual(triangle.classify_triangle(10,10,3),"isosceles")
         self.assertEqual(triangle.classify_triangle(3,4,5),"right")
         self.assertEqual(triangle.classify_triangle(3,3,3),"equilateral")
         self.assertEqual(triangle.classify_triangle(24,10,26),"right")
+        
 if __name__ == "__main__":
     unittest.main()
